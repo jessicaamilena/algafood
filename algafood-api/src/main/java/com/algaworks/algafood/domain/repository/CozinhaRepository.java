8ef571959809ea97801fd9c9a0fbf8fc30112432
Milena <jessicaa.milena@gmail.com>
2021-11-19 11:09:57 -0400

    Commit controllers

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -6,8 +6,8 @@ import com.algaworks.algafood.model.Cozinha;
 
 public interface CozinhaRepository {
 
-	List<Cozinha> todas();
-	Cozinha porId(Long id);
+	List<Cozinha> listar();
+	Cozinha buscar(Long id);
 	Cozinha adicionar(Cozinha cozinha);
 	void remover(Cozinha cozinha);
 }
--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -16,12 +16,12 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 	private EntityManager manager;
 	
 	@Override
-	public List<Cozinha> todas(){
+	public List<Cozinha> listar(){
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
 	
 	@Override
-	public Cozinha porId(Long id) {
+	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
@@ -34,8 +34,7 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 	@Override
 	@Transactional
 	public void remover(Cozinha cozinha) {
-		cozinha = porId(cozinha.getId());
+		cozinha = buscar(cozinha.getId());
 		manager.remove(cozinha);
-	}
-	
+	}	
 }
