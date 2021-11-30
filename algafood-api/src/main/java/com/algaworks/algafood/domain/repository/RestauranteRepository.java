e8b84ba670e4c604ffc16e66ae40cb08070a7ac7
Milena <jessicaa.milena@gmail.com>
2021-11-30 12:13:39 -0400

    commit JPQL-customizada-com-Query

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.algaworks.algafood.model.Restaurante;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 
 	List<Restaurante> findByTaxaFreteBetween (BigDecimal taxaInicial, BigDecimal taxaFinal) ;
 	
-	List<Restaurante> findByNomeContainingAndCozinhaId (String nome, Long cozinha);
+	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultaPorNome (String nome, @Param("id") Long cozinha);
 	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 	
