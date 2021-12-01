e1a2ab6b0c8828024babcd9b8f51b6f6c08a4e08
Milena <jessicaa.milena@gmail.com>
2021-12-01 13:43:40 -0400

    commit consulta-Criteria-API

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/RestauranteRepositoryImpl.java
@@ -1,14 +1,13 @@
 package com.algaworks.algafood.domain.service;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import com.algaworks.algafood.model.Restaurante;
 
@@ -21,30 +20,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
-		StringBuilder jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
 		
-		Map<String, Object> parametros = new HashMap<>();
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("nome", "%" + nome + "%");
-		}
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
 		
-		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		
+		TypedQuery<Restaurante>query = manager.createQuery(criteria);
 		return query.getResultList();
 		
 	}
