5273a70cdf76bf74a810ccd0df18709ed98fd4a0
Milena <jessicaa.milena@gmail.com>
2021-12-02 09:21:42 -0400

    commit restricao-where-com-criteria-API

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/RestauranteRepositoryImpl.java
@@ -7,6 +7,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.stereotype.Repository;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 import com.algaworks.algafood.model.Restaurante;
@@ -24,7 +27,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		Predicate nomePredicate =  builder.like(root.get("nome"), "%" + nome + "%");
+		
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFreteInicial"), taxaFreteInicial);
+		
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFreteFinal"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
 		TypedQuery<Restaurante>query = manager.createQuery(criteria);
 		return query.getResultList();
